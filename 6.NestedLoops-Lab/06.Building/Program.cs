c2837725c07965a002ec7f0210195a08e981c01d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:55:46 +0300

    Add files via upload

---
+++ 6.NestedLoops-Lab/06.Building/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06.Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int floor =  int.Parse(Console.ReadLine());
+            int room= int.Parse(Console.ReadLine());
+
+            for(int f = floor; f >= 1; f--)
+            {
+                  for(int r = 0; r < room; r++) 
+                  { 
+                   
+                        if(f == floor) 
+                        {
+                             Console.Write($"L{f}{r} ");
+                        }
+                        else if(f%2==0)
+                        {
+                             Console.Write($"О{f}{r} ");
+                        
+                          }
+                        else if(f % 2 != 0) 
+                        { 
+                             Console.Write($"А{f}{r} ");
+
+                        }
+                   }
+              Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
