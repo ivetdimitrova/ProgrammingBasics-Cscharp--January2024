add855612f66f33821d1d01952da107aee82853d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:18:19 +0300

    Add files via upload

---
+++ 4.For Loop - Lab/07.SumNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _07.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 0; i < countOfNumbers; i++) 
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 4.For Loop - Lab/08.Numbersequence/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08.Numbersequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int countOfNumbers = int.Parse(Console.ReadLine());
+
+            int max = int.MinValue;
+            int min = int.MaxValue ;
+
+            for(int i = 0;i < countOfNumbers; i++) 
+            {
+                int number = int.Parse(Console.ReadLine());
+                if(number > max)
+                {
+                    max = number;
+                }
+                 if (number < min) 
+                { 
+                   min = number;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+
+        }
+    }
+}
