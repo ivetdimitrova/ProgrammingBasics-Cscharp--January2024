cb4536d3071ff9558bf181666b757b76245a8539
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 15:19:34 +0300

    Add files via upload

---
+++ 1.1.FirstStepsInCoding-Exersice/01.USDToBGN/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace USDToBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bg = usd* 1.79549;
+            Console.WriteLine(bg);
+
+        }
+    }
+}
---
+++ 1.1.FirstStepsInCoding-Exersice/03.DepositCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          double depositSum = double.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                double procent = double.Parse(Console.ReadLine()) / 100;
+
+            double sum = depositSum + time *((depositSum* procent) / 12);
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
