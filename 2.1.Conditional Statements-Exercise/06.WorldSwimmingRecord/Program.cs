2a901ff436e0dd2f3a28634b4d50c99741f7a3f9
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 15:50:58 +0300

    Add files via upload

---
+++ 2.1.Conditional Statements-Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Рекордът в секунди – реално число в интервала[0.00 … 100000.00]
+            double recordinseconds = double.Parse(Console.ReadLine());
+            //.Разстоянието в метри – реално число в интервала[0.00 … 100000.00]
+            double meters = double.Parse(Console.ReadLine());   
+            //.Времето в секунди, за което плува разстояние от 1 м. - реално число в интервала[0.00 … 1000.00]
+            double secondForOneMeters = double.Parse(Console.ReadLine());
+            double FirstTime = meters * secondForOneMeters;
+             double meteres2 =Math.Floor( meters / 15);
+            double SEcondTime = meteres2 * 12.5;
+            double realTime = FirstTime+ SEcondTime;
+            if (realTime < recordinseconds)
+            { //Ако Иван е подобрил Световния рекорд(времето му е по-малко от рекорда) отпечатваме:
+                Console.WriteLine($" Yes, he succeeded! The new world record is {realTime:f2} seconds.");
+            }
+            else
+            { //Ако НЕ е подобрил рекорда(времето му е по-голямо или равно на рекорда) отпечатваме:
+                double lastseconds = realTime - recordinseconds;
+                Console.WriteLine($"No, he failed! He was {lastseconds:f2} seconds slower.");
+            }
+        }
+    }
+}
