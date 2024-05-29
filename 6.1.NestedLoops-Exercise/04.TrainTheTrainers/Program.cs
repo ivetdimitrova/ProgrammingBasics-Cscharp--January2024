a3fe95f119ac7625332188806db82df3d103058b
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 17:02:19 +0300

    Add files via upload

---
+++ 6.1.NestedLoops-Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryNumber = int.Parse(Console.ReadLine());
+         
+            string presentationName = Console.ReadLine();
+            int presentationsNumber = 0;
+            double allSum = 0;
+
+            while (presentationName != "Finish")
+            {
+                presentationsNumber++;
+                double obhtasumanaoc = 0;
+                for(int i = 0; i < juryNumber; i++)
+                {
+                    double ocenki = double.Parse(Console.ReadLine());
+                    obhtasumanaoc+= ocenki;
+                }
+                double averageSumForOne = obhtasumanaoc/juryNumber;
+                allSum+= averageSumForOne;
+                Console.WriteLine($"{presentationName} - {averageSumForOne:f2}.");
+                
+
+                presentationName = Console.ReadLine();
+            }
+            Console.WriteLine($"Student's final assessment is {allSum/presentationsNumber:f2}.");
+        }
+    }
+}
---
+++ 6.1.NestedLoops-Exercise/06.CinemaTickets/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+
+namespace _06.CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string filmName = Console.ReadLine();
+
+            int countStudentTickets = 0;
+            int countStandardTickets = 0;
+            int countKidTickets = 0;
+
+            while (filmName != "Finish")
+            {
+                int freeTickets = int.Parse(Console.ReadLine());
+
+                string type = string.Empty;
+                int buyTickets = 0;
+                while (buyTickets < freeTickets)
+                {
+                    type = Console.ReadLine();
+
+                    if (type == "End")
+                        break;
+
+                    buyTickets++;
+
+                    if (type == "student")
+                        countStudentTickets++;
+                    else if (type == "standard")
+                       countStandardTickets++;
+                    else
+                        countKidTickets++;
+                }
+
+                Console.WriteLine($"{filmName} - {(double)buyTickets / freeTickets *100:f2}% full.");
+
+                filmName = Console.ReadLine() ;
+            }
+
+            int totalTickets = countStudentTickets + countKidTickets + countStandardTickets;
+
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{(double)countStudentTickets / totalTickets * 100:f2}% student tickets.");
+            Console.WriteLine($"{(double)countStandardTickets/ totalTickets * 100 :f2}% standard tickets.");
+            Console.WriteLine($"{(double)countKidTickets / totalTickets * 100 :f2}% kids tickets.");
+           
+        }
+    }
+}
