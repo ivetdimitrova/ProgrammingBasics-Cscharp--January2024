bc64120757fcda215cee3f62594554200552fa73
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:21:09 +0300

    Add files via upload

---
+++ 4.1.For Loop - Lab/01.NumbersEndingin7/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.NumbersEndingin7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          for(int i=7; i<=997; i+=10) 
+          {
+                Console.WriteLine(i);
+          }
+        }
+    }
+}
