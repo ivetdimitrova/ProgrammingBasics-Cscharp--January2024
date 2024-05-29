b8f67a68929ba66dc098f047ac9c2e5eec9c1766
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 15:58:28 +0300

    Add files via upload

---
+++ 3.ConditionalStatementsAdvanced-Lab/07.WorkingHours/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _07.WorkingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //    офисът на фирма е отворен, като работното време на офисът е от 10-18 часа,
+            //    от понеделник до събота включително
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (hour >= 10 && hour<= 18)
+            {
+                switch (day)
+                {
+
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    case "Saturday":
+                        Console.WriteLine("open");
+                        break;
+                    default:
+                        Console.WriteLine("closed");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
