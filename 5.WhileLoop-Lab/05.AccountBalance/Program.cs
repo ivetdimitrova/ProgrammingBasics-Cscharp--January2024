be6bf5e8b836f5e08146d92630b2e20bb667cfc9
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:45:48 +0300

    Add files via upload

---
+++ 5.WhileLoop-Lab/05.AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double allMoney = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double money = double.Parse(input);
+
+                if (money<0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                
+                    Console.WriteLine($"Increase: {money:f2}");
+                    allMoney+= money;
+               
+                input= Console.ReadLine();
+            }
+            
+            Console.WriteLine($"Total: {allMoney:f2}");
+        }
+    }
+}
