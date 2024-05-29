36cd34b2de436bfe2dd4905fdfbc4a7a713beb05
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 17:01:28 +0300

    Add files via upload

---
+++ 6.1.NestedLoops-Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = int.Parse(Console.ReadLine());
+
+            int num = 0; 
+
+            for(int row = 1; row <= number; row++) 
+            { 
+                for(int positions = 1;positions<=row;positions++)
+                {
+                    num++;
+                    if (num > number)
+                        break;
+
+                    Console.Write(num + " ");
+                }
+               Console.WriteLine();
+            }
+          
+        }
+    }
+}
---
+++ 6.1.NestedLoops-Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int startPosition = int.Parse(Console.ReadLine());
+           int endPosition = int.Parse(Console.ReadLine());
+
+
+            for(int i = startPosition; i < endPosition; i++)
+            {
+                string num = i.ToString();
+                int evenSum = 0;
+                int oddSum = 0;
+                for(int j = 0; j < num.Length; j++)
+                {
+                    int digit = (int)num[j];
+
+                    if(j%2==0)
+                        evenSum+= digit;
+                    if(j%2!=0)
+                        oddSum+= digit;
+                }
+                if(evenSum == oddSum)
+                    Console.Write(num + " ");
+            }
+        }
+    }
+}
