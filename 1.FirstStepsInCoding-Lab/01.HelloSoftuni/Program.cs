1a74c7d355e357546cd8c35695f8e35190ce80cc
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 15:14:04 +0300

    Add files via upload

---
+++ 1.FirstStepsInCoding-Lab/01.HelloSoftuni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloSoftuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
---
+++ 1.FirstStepsInCoding-Lab/03.RectangleArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+
+            int area = sideA * sideB;
+            Console.WriteLine(area);
+        }
+    }
+}
