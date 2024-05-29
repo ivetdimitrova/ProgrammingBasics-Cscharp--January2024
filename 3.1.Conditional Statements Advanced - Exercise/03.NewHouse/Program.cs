9cc59b1cd174e08bd46cecddb5d8c2ee19ec7b39
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:03:10 +0300

    Add files via upload

---
+++ 3.1.Conditional Statements Advanced - Exercise/03.NewHouse/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _03.NewHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string type = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+
+
+
+            double price = 0;
+            double discount = 0;
+            double overpriced = 0;
+            //   Съществуват следните отстъпки:
+            switch (type)
+            {
+                case "Roses":
+                    price =count *5.00;
+                    if (count > 80)
+                    {
+                        discount = price * 0.1;
+                    }
+                    break;
+                case "Dahlias":
+                    price =count* 3.80;
+                    if (count > 90)
+                    {
+                        discount = price * 0.15;
+                    }
+                    break;
+                case "Tulips":
+                    price = count * 2.80;
+                    if (count > 80)
+                    {
+                        discount = price * 0.15;
+                    }
+                    break;
+                case "Narcissus":
+                    price = count * 3;
+                    if (count < 120)
+                    {
+                        overpriced = price * 0.15;
+                    }
+                    break;
+                case "Gladiolus":
+                    price = count * 2.50;
+                    if (count < 80)
+                    {
+                        overpriced = price * 0.20;
+                    }
+                    break;
+            }
+
+            double totalCost = price - discount + overpriced;
+            if (budget >= totalCost)
+            {
+
+                double lastmoney2 = budget - totalCost;
+
+
+
+
+                Console.WriteLine($"Hey, you have a great garden with {count} {type} and {lastmoney2:f2} leva left.");
+
+
+            }
+            else if(budget < totalCost )
+            {
+
+                double neededmoney2 = totalCost - budget;
+
+
+                Console.WriteLine($"Not enough money, you need {neededmoney2:f2} leva more.");
+
+
+            }
+
+
+        }
+    }
+}
---
+++ 3.1.Conditional Statements Advanced - Exercise/04.FishingBoat/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _04.FishingBoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+// Бюджет на групата – цяло число в интервала[1…8000]
+           int budget = int.Parse(Console.ReadLine());
+//· Сезон – текст: "Spring", "Summer", "Autumn", "Winter"
+           string season = Console.ReadLine();
+//· Брой рибари – цяло число в интервала[4…18]
+            int fisherman = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            switch (season)
+            {
+                case "Spring":
+                    price += 3000;
+                    break;
+                case "Summer":
+                    case  "Autumn":
+                    price+=4200;
+                    break;
+                case "Winter":
+                    price+=2600;
+                    break;
+            }
+
+            if(fisherman <= 6)
+            {
+                price = price  * 0.9;
+            }
+            else if(fisherman >= 7 && fisherman <= 11)
+            {
+                price = price  * 0.85;
+
+            }
+            else if (fisherman >= 12)
+            {
+                price = price * 0.75;
+            }
+            
+            if(fisherman % 2 == 0 && season != "Autumn")
+            {
+                    price = price  * 0.95;
+            }
+
+            if(budget >= price)
+            {
+                double lastmoney = budget - price;
+                Console.WriteLine($"Yes! You have {lastmoney:f2} leva left.");
+            }
+            else
+            {
+                double needemoney = price - budget;
+                Console.WriteLine($"Not enough money! You need {Math.Abs(needemoney):f2} leva.");
+            }
+        }
+    }
+}
