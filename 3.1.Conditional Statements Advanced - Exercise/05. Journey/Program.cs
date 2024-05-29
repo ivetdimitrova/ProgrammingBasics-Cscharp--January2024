b699f7f287bbbee3d978aa9d51c76f5de1a3f55e
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:03:56 +0300

    Add files via upload

---
+++ 3.1.Conditional Statements Advanced - Exercise/05. Journey/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _05._Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double  budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double price = 0;
+            //double priceForAll = 0;
+            string place = " ";
+            if (budget <= 100)
+            {
+                switch (season)
+                {
+                    case "summer":
+                        price = budget * 0.3;
+                        place = "Camp";
+                        break;
+                    case "winter":
+                        price = budget * 0.7;
+                        place = "Hotel";
+                        break;
+                }
+                //priceForAll = budget - priceWithDiscount;
+                Console.WriteLine("Somewhere in Bulgaria");
+                Console.WriteLine($"{place} - {price:f2}");
+            }
+            else if (budget <= 1000)
+            {
+                switch (season)
+                {
+                    case "summer":
+                        price = budget  * 0.4;
+                        place = "Camp";
+                        break;
+                    case "winter":
+                        price = budget  * 0.8;
+                        place = "Hotel";
+                        break;
+                }
+                //priceForAll = budget - priceWithDiscount;
+
+                Console.WriteLine("Somewhere in Balkans");
+                Console.WriteLine($"{place} - {price:f2}");
+            }
+           else  if (budget > 1000)
+            {
+
+                price = budget * 0.9;
+                //priceForAll = budget - priceWithDiscount;
+
+                Console.WriteLine("Somewhere in Europe");
+                Console.WriteLine($"Hotel - {price:f2}");
+            }
+        }
+    }
+}
---
+++ 3.1.Conditional Statements Advanced - Exercise/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _06.OperationsBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+
+                string sentence = " ";
+                double sum = 0;
+            if(n1!=0 && n2!=0) 
+            {
+                switch (symbol)
+                {
+                    case '+':
+                        sum = n1 + n2;
+                        if (sum%2==0)
+                        {
+                            sentence = $"{n1} + {n2} = {sum} - even";
+                               
+                        }
+                        else
+                        {
+                            sentence = $"{n1} + {n2} = {sum} - odd";
+                        }
+                        break;
+                    case '-':
+                        sum = n1 - n2;
+                        if (sum%2==0)
+                        {
+                            sentence = $"{n1} - {n2} = {sum} - even";
+
+                        }
+                        else
+                        {
+                            sentence = $"{n1} - {n2} = {sum} - odd";
+                        }
+                        break;
+                    case '*':
+                        sum = n1 * n2;
+                        if (sum%2==0)
+                        {
+                            sentence = $"{n1} * {n2} = {sum} - even";
+
+                        }
+                        else
+                        {
+                            sentence = $"{n1} * {n2} = {sum} - odd";
+                        }
+                        break;
+                    case '/':
+                        sum = n1 / n2;
+                       sentence = $"{n1} / {n2} = {sum:f2} ";
+                        break;
+                    case '%':
+                        sum = n1 % n2;
+                        sentence = $"{n1} % {n2} = {sum} ";
+                        break;
+                }
+            }
+            else
+            {
+                sentence = $"Cannot divide {n1} by zero";
+            }
+            Console.WriteLine(sentence);
+        }
+    }
+}
