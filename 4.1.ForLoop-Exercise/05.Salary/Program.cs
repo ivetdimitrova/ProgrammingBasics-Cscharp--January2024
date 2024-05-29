0ad2c87a422b1fafda83466d03815fed94c6e855
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:30:45 +0300

    Add files via upload

---
+++ 4.1.ForLoop-Exercise/05.Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int openedTabs = int.Parse(Console.ReadLine());
+           double salary = double.Parse(Console.ReadLine());
+
+            //double fine = 0;
+
+            for(int i = 0 ; i < openedTabs; i++) 
+            {
+                string nameTab = Console.ReadLine();
+                switch(nameTab)
+                {
+                    case "Facebook":
+                        salary -=150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+               
+                if(salary<=0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+
+            if(salary >=1)
+            {
+                Console.WriteLine($"{salary}");
+            }
+            
+
+        }
+    }
+}
---
+++ 4.1.ForLoop-Exercise/06.Oscars/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _06.Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //string actorsName = Console.ReadLine();
+           // double points = double.Parse(Console.ReadLine());
+           // int raters = int.Parse(Console.ReadLine());
+
+           // for(int i = 0; i < raters; i++)
+           // {
+           //     string raterName = Console.ReadLine();
+           //     double point = double.Parse(Console.ReadLine());
+
+           //     int countOfLeters = raterName.Length;
+
+           //     points =  points + ((point * countOfLeters)/2);
+
+           // }
+           // if(points > 1250.5)
+           // {
+           //     Console.WriteLine($"Congratulations, {actorsName} got a nominee for leading role with {points}!");
+           // }
+           // else
+           // {
+
+           //     Console.WriteLine($"Sorry, {actorsName} you need {Math.Abs(points - 1250.5)} more!");
+           // }
+
+            string actorName = Console.ReadLine();
+            double pointsForAcademy = double.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                string name = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+                pointsForAcademy += (name.Length) * points / 2;
+
+                if (pointsForAcademy>= 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {pointsForAcademy:f1}!");
+                    break;
+                }
+            }
+
+            if (pointsForAcademy < 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {Math.Abs(pointsForAcademy - 1250.5):f1} more!");
+            }
+        }
+    }
+}
