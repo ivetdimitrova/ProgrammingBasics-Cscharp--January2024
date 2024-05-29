a74a2c0a53851e31bb76bdfbccf3aa8e57823eee
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:45:08 +0300

    Add files via upload

---
+++ 5.WhileLoop-Lab/04.Sequence2k+1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Sequence2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int border = int.Parse(Console.ReadLine());
+
+            int number = 1;
+            while(number<= border)
+            {
+                Console.WriteLine(number);
+                number = number * 2 +1;
+            }
+        }
+    }
+}
