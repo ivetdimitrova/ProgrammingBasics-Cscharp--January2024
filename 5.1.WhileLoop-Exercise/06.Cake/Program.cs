c4fcd089c2a53a5f71aaa10b917511ffe13fd2dc
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:50:37 +0300

    Add files via upload

---
+++ 5.1.WhileLoop-Exercise/06.Cake/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeSize = int.Parse(Console.ReadLine()) * int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while (input != "STOP")
+            {
+                cakeSize -= int.Parse(input);
+
+                if (cakeSize <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(cakeSize)} pieces more.");
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "STOP")
+                Console.WriteLine($"{cakeSize} pieces are left.");
+        }
+    }
+}
---
+++ 5.1.WhileLoop-Exercise/07.Moving/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int heigth = int.Parse(Console.ReadLine());
+
+            int freePlace = width* length * heigth;
+            int filledPlace = 0;
+
+            string input = Console.ReadLine(); 
+        
+            while(input != "Done")
+            {
+              int countOfCartons = int.Parse(input);
+                filledPlace += countOfCartons;
+                if(filledPlace > freePlace)
+                {
+                    Console.WriteLine($"No more free space! You need {filledPlace - freePlace} Cubic meters more.");
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+            if(freePlace > filledPlace)
+            {
+                Console.WriteLine($"{freePlace - filledPlace} Cubic meters left.");
+            }
+        }
+    }
+}
