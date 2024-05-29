b10740860950d1d38713faf3de4a88c2ee574dd1
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:06:14 +0300

    Add files via upload

---
+++ 3.1.Conditional Statements Advanced - Exercise/09.SkiTrip/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace _09.SkiTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfStay = int.Parse(Console.ReadLine());
+            string roomType = Console.ReadLine();
+            string score = Console.ReadLine();
+
+            double roomForOnePerson = 18.00;
+            double apartment = 25.00;
+            double presidentAp = 35.00;
+
+
+            int nightsOfStay = daysOfStay - 1;
+            double roomForOnePersonPrice = roomForOnePerson * nightsOfStay;
+            double apartmentPrice = apartment * nightsOfStay;
+            double presidentApPrice = presidentAp * nightsOfStay;
+
+            switch (roomType)
+            {
+                case "apartment":
+
+                    if (daysOfStay < 10)
+
+                        apartmentPrice *= 0.70;
+
+                    else if (daysOfStay >= 10 && daysOfStay <= 15)
+
+                        apartmentPrice *= 0.65;
+
+                    else if (daysOfStay > 15)
+
+                        apartmentPrice *= 0.50;
+
+                    if (score == "positive")
+                    {
+                        double final = apartmentPrice + apartmentPrice * 0.25;
+                        Console.WriteLine($"{final:f2}");
+                    }
+                    else if (score == "negative")
+                    {
+                        double final = apartmentPrice - apartmentPrice * 0.10;
+                        Console.WriteLine($"{final:f2}");
+                    }
+
+                    break;
+
+
+                case "president apartment":
+
+                    if (daysOfStay < 10)
+
+                        presidentApPrice *= 0.90;
+
+                    else if (daysOfStay >= 10 && daysOfStay <= 15)
+
+                        presidentApPrice *= 0.85;
+
+                    else if (daysOfStay > 15)
+
+                        presidentApPrice *=0.80;
+
+                    if (score == "positive")
+                    {
+                        double final = presidentApPrice + presidentApPrice * 0.25;
+                        Console.WriteLine($"{final:f2}");
+                    }
+                    else if (score == "negative")
+                    {
+                        double final = presidentApPrice - presidentApPrice * 0.10;
+                        Console.WriteLine($"{final:f2}");
+                    }
+                    break;
+
+                case "room for one person":
+                    if (score == "positive")
+                    {
+                        double final = roomForOnePersonPrice + roomForOnePersonPrice * 0.25;
+                        Console.WriteLine($"{final:f2}");
+                    }
+                    else if (score == "negative")
+                    {
+                        double final = roomForOnePersonPrice - roomForOnePersonPrice * 0.10;
+                        Console.WriteLine($"{final:f2}");
+                    }
+
+                    break;
+            }
+        }
+    }
+}
