3f6b06b1d32b778a68f30ff97cab3e0b19954fcd
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:00:28 +0300

    Add files via upload

---
+++ 3.ConditionalStatementsAdvanced-Lab/11.FruitShop/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _11.FruitShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double count = double.Parse(Console.ReadLine());
+
+            double priceForFood = 0;
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    switch(fruit)
+                    {
+                        case "banana":
+                            priceForFood = 2.50;
+                            break;
+                        case "apple":
+                            priceForFood = 1.20;
+                            break;
+                        case "orange":
+                            priceForFood = 0.85;
+                            break;
+                        case "grapefruit":
+                            priceForFood = 1.45;
+                            break;
+                        case "kiwi":
+                            priceForFood = 2.70;
+                            break;
+                        case "pineapple":
+                            priceForFood = 5.50;
+                            break;
+                        case "grapes":
+                            priceForFood = 3.85;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    switch(fruit)
+                    {
+                        case "banana":
+                            priceForFood = 2.70;
+                            break;
+                        case "apple":
+                            priceForFood = 1.25;
+                            break;
+                        case "orange":
+                            priceForFood = 0.90;
+                            break;
+                        case "grapefruit":
+                            priceForFood = 1.60;
+                            break;
+                        case "kiwi":
+                            priceForFood =3.00;
+                            break;
+                        case "pineapple":
+                            priceForFood = 5.60;
+                            break;
+                        case "grapes":
+                            priceForFood = 4.20;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            double priceForAll = count * priceForFood;
+
+            if(priceForAll!=0)
+            {
+                Console.WriteLine($"{priceForAll:f2}");
+            }
+        }
+    }
+}
---
+++ 3.ConditionalStatementsAdvanced-Lab/12.TradeCommissions/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace _12.TradeCommissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double value = double.Parse(Console.ReadLine());
+
+            double comm = 0;
+            switch (town)
+            {
+
+                case "Sofia":
+                    if (value < 0)
+                    {
+                        Console.WriteLine("error");
+                    }
+                    if (0 <= value && value <= 500)
+                    {
+                        comm = (value *5)/100;
+                    }
+                    else if (500<= value && value <= 1000)
+                    {
+                        comm = (value * 7)/100;
+                    }
+                    else if (1000<= value && value <= 10000)
+                    {
+                        comm = (value *8)/100;
+                    }
+                    else if (value > 10000)
+                    {
+                        comm = (value * 12)/100;
+                    }
+
+
+                    break;
+
+                case "Varna":
+                    if (value < 0)
+                    {
+                        Console.WriteLine("error");
+                    }
+                    if (0 <= value && value <= 500)
+                    {
+                        comm = (value *4.5)/100;
+                    }
+                    else if (500<= value && value <= 1000)
+                    {
+                        comm = (value * 7.5)/100;
+                    }
+                    else if (1000<= value && value <= 10000)
+                    {
+                        comm = (value *10)/100;
+                    }
+                    else if (value > 10000)
+                    {
+                        comm = (value *13)/100;
+                    }
+
+                    break;
+
+                case "Plovdiv":
+                    if (value < 0)
+                    {
+                        Console.WriteLine(" error");
+                    }
+                    else if (0 <= value && value <= 500)
+                    {
+                        comm = (value *5.5)/100;
+                    }
+                    else if (500<= value && value <= 1000)
+                    {
+                        comm = (value * 8)/100;
+                    }
+                    else if (1000<= value && value <= 10000)
+                    {
+                        comm = (value *12)/100;
+                    }
+                    else if (value > 10000)
+                    {
+                        comm = (value * 14.5)/100;
+                    }
+                    break;
+            }
+            switch (town)
+            {
+                case "Sofia":
+                case "Plovdiv":
+                case "Varna":
+                 if (value >= 0)
+                 { 
+                   Console.WriteLine($"{comm:f2}");
+                 }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+        }
+    }
+}
