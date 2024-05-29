4944b578e88c4b62883027c787ba96cdb934d496
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:05:26 +0300

    Add files via upload

---
+++ 3.1.Conditional Statements Advanced - Exercise/07.HotelRoom/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _07.HotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string period = Console.ReadLine();
+            int time = int.Parse(Console.ReadLine());
+
+            double apartment = 0;
+            double studio = 0;
+            double priceForStudio = 0;
+            double priceForApartment = 0;
+            //double studio1 = 0;
+            //double 1 = 0;
+            switch (period)
+            {     
+                
+                case "May":
+                case "October":
+                    apartment =  65;
+                        studio = 50;
+                    if(time >= 14)
+                    {
+                         studio = studio - (studio *  0.3);
+                        //studio1 = time * studio;
+
+                        apartment = apartment - (apartment * 0.1);
+                       // apartmen
+                    }
+                    else if(time >= 7)
+                    {
+                        studio = studio - (studio *  0.05);
+                       //priceForStudio = time * studio;
+                       
+                    }
+                    break;
+                case "June":
+                case "September":
+                    apartment =  68.70;
+                    studio =  75.20;
+                   
+                    if (time >= 14)
+                    {
+                        studio = studio - (studio *  0.2);
+                        //priceForStudio = time * studio;
+                       
+                        
+                           apartment = apartment - (apartment * 0.1);
+                        
+                    }
+                    break;
+                case "July":
+                    case "August":
+                    apartment = 77;
+                    studio =  76;
+                    if (time >= 14)
+                    {
+                        apartment = apartment - (apartment * 0.1);
+                    }
+                    break;
+              
+            }
+                    priceForApartment = time * apartment;
+            priceForStudio = time * studio;
+                    
+            Console.WriteLine($"Apartment: {priceForApartment:f2} lv.");
+            Console.WriteLine($"Studio: {priceForStudio:f2} lv.");
+
+        }
+    }
+}
---
+++ 3.1.Conditional Statements Advanced - Exercise/08.OnTimefortheExam/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.Metrics;
+using System.IO;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace _08.OnTimefortheExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMinutes = int.Parse(Console.ReadLine());
+
+
+
+            // превръщам часа на започване на изпита в минути и прибавям минутите на започване на изпита
+            int examTimeInMinutes = examHour * 60 + examMinutes;
+            // превръщам часа на явяване за изпита в минути и прибавям минутите на явяване за изпита
+            int arrivalTimeInMinutes = arrivalHour * 60 + arrivalMinutes;
+            // намирам разликата във времената
+            int timeDifference = arrivalTimeInMinutes - examTimeInMinutes;
+
+            // започвам проверки за разликата във времената 
+            // “Early”, ако студентът пристига повече от 30 минути преди часа на изпита.
+            if (timeDifference < -30)
+            {
+                Console.WriteLine("Early");
+            }
+            // “On time”, ако студентът пристига точно в часа на изпита или до 30 минути по-рано.
+            else if (timeDifference <= 0)
+            {
+                Console.WriteLine("On time");
+            }
+            // “Late”, ако студентът пристига по-късно от часа на изпита.
+            else
+            {
+                Console.WriteLine("Late");
+            }
+
+            if (timeDifference != 0)
+            {
+                int hours = Math.Abs(timeDifference / 60);
+                int minutes = Math.Abs(timeDifference % 60);
+
+                if (hours > 0)
+                {
+                    Console.WriteLine($"{hours}:{minutes:D2} hours {(timeDifference < 0 ? "before" : "after")} the start");
+                }
+                else
+                {
+                    Console.WriteLine($"{minutes} minutes {(timeDifference < 0 ? "before" : "after")} the start");
+                }
+
+            }
+           
+        }
+    }
+}
