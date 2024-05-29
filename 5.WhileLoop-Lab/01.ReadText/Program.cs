e7bd1f7e0fb36a974058d2f645626c797e1a0bf4
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:38:39 +0300

    Add files via upload

---
+++ 5.WhileLoop-Lab/01.ReadText/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.ReadText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            while(text != "Stop") 
+            { 
+                 Console.WriteLine(text);
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
