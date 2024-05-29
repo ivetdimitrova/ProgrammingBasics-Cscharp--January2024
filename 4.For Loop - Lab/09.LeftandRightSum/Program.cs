4e97a96bd7daa907bee15c3b193aeae0f680dc15
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:19:24 +0300

    Add files via upload

---
+++ 4.For Loop - Lab/09.LeftandRightSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _09.LeftandRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0;i< n;i++) 
+            { 
+              int numbers1 = int.Parse(Console.ReadLine());
+                sum1+= numbers1;
+            }
+            for(int i = 0; i < n; i++)
+            {
+                int numbers2 = int.Parse(Console.ReadLine());
+                sum2+= numbers2;
+            }
+
+            if(sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = { sum1}");
+            }
+            else
+            {
+                int difference = sum1 - sum2;
+                Console.WriteLine($"No, diff = { Math.Abs(difference)}");
+            }
+        }
+    }
+}
---
+++ 4.For Loop - Lab/10.OddEvenSum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _10.OddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumEven = 0; 
+            int sumOdd = 0;
+
+            for(int i = 1 ; i <= n; i++) 
+            { 
+                int number = int.Parse(Console.ReadLine());
+                if(i % 2 == 0)
+                {
+                    sumEven += number;
+                }
+                else
+                {
+                    sumOdd+= number;
+                }
+            }
+
+            if(sumEven == sumOdd) 
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($" Sum = {sumOdd}");
+            }
+            else
+            {
+                int difference = sumEven - sumOdd;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(difference)}");
+            }
+        }
+    }
+}
