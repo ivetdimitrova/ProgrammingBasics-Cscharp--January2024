486ca7eeb491064377fc51e180f7e66945df49ed
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 16:02:00 +0300

    Add files via upload

---
+++ 3.1.Conditional Statements Advanced - Exercise/01.Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+
+            int places = rows * colums;
+            double income = 0.0;
+            switch (type)
+            {
+                case "Premiere":
+                    income = places * 12.00;
+                    break;
+                case "Normal":
+                    income = places * 7.50;
+                    break;
+                case " Discount":
+                    income = places * 5.00;
+                    break;
+            }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
---
+++ 3.1.Conditional Statements Advanced - Exercise/02.SummerOutfit/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02.SummerOutfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //  спрямо времето
+                // от денонощието и градусите да препоръча на Виктор какви дрехи да си облече. Вашия приятел има различни
+                //   планове за всеки етап от деня, които изискват и различен външен вид, тях може да видите от таблицата.
+  
+                int degress = int.Parse(Console.ReadLine());
+                string time = Console.ReadLine();
+            switch(time) 
+            {
+                case "Morning":
+                    if(10<= degress && degress<= 18) 
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your Sweatshirt and Sneakers.");
+                    }
+                    else if (degress <= 24) 
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your Shirt and Moccasins.");
+
+                    }
+                    else if(degress<= 25)
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your T-Shirt and Sandals.");
+
+                    }
+                    break;
+                case "Afternoon":
+                    if (10<= degress && degress<= 18)
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your Shirt and Moccasins.");
+                    }
+                    else if (degress <= 24)
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your T-Shirt and Sandals.");
+
+                    }
+                    else if (degress<= 25)
+                    {
+                        Console.WriteLine($"It's {degress} degrees, get your Swim Suit and Swim Suit.");
+
+                    }
+                    break;
+                case "Evening":
+                    Console.WriteLine($"It's {degress} degrees, get your Shirt and Moccasins.");
+                    break;
+
+
+            }
+        }
+    }
+}
