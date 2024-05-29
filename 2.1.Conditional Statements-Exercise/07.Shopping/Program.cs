c20e661ad197f4bcef78dbaf697e07a974102923
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-05-29 15:52:03 +0300

    Add files via upload

---
+++ 2.1.Conditional Statements-Exercise/07.Shopping/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Бюджетът на Петър - реално число-
+            double budget = double.Parse(Console.ReadLine());
+            //Броят видеокарти - цяло число- cena 250 лв./бр.
+            int videocards = int.Parse(Console.ReadLine());
+            double priceForVideoCards = videocards * 250;
+            //Броят процесори - цяло число - cena 35% от цената на закупените видеокарти/бр.
+           int  processor = int.Parse(Console.ReadLine());
+            double priceForProcessor = processor *(priceForVideoCards * 0.35);
+            //Броят рам памет -цяло число- cena 10% от цената на закупените видеокарти/бр.
+               int ram = int.Parse(Console.ReadLine());
+            double priceForRam = ram * (priceForVideoCards * 0.1);
+
+            double priceForAll = priceForVideoCards + priceForProcessor + priceForRam;
+            if(videocards>processor)
+            {
+                priceForAll = priceForAll - (priceForAll * 0.15);
+            }
+            // Ако бюджета е достатъчен:
+            if (priceForAll <= budget)
+            {
+                double lastmoney = budget - priceForAll;
+                Console.WriteLine($"You have {lastmoney:f2} leva left!");
+            }
+            else // Ако сумата надхвърля бюджета:
+            {
+                double neededmoney = priceForAll - budget;
+                Console.WriteLine($"Not enough money! You need {neededmoney:f2} leva more!");
+            }
+        }
+    }
+}
---
+++ 2.1.Conditional Statements-Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Име на сериал – текст
+            string nameOfSerial = Console.ReadLine();
+            // Продължителност на епизод – цяло число- 
+            int minutesOfSerial = int.Parse(Console.ReadLine());
+            // Продължителност на почивката – цяло число - 
+            int minutesOfBreak = int.Parse(Console.ReadLine());
+            // Времето за обяд ще бъде 1/8 от времето за почивка
+            double TimeForEat = minutesOfBreak/ 8.0;
+            //времето за отдих ще бъде 1/4 от времето за почивка.
+            double timeForRelax = minutesOfBreak / 4.0;
+
+            double neededTime = minutesOfSerial + TimeForEat + timeForRelax;
+
+            double lastTime = Math.Abs(neededTime- minutesOfBreak);
+
+            if (neededTime <= minutesOfBreak)
+            {
+                Console.WriteLine($"You have enough time to watch {nameOfSerial} and left with {Math.Ceiling(lastTime)} minutes free time.");
+            }
+            else
+            {
+               
+                Console.WriteLine($"You don't have enough time to watch {nameOfSerial}, you need {Math.Ceiling(lastTime)} more minutes.");
+            }
+        }
+    }
+}
